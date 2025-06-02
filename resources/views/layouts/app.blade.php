77161da12e84834f66ccaa6310774acb70bd516a
Siti Zulaikha <siti@unblk.io>
2025-06-02 19:01:31 +0800

    update url() calls for static assets

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,13 +19,17 @@
     <script src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
 
     <!-- Favicon icon -->
-    <link rel="icon" href="{{url('/assets/images/favicon.ico')}}" type="image/x-icon">
+    {{-- <link rel="icon" href="{{url('/assets/images/favicon.ico')}}" type="image/x-icon"> --}}
+    <link rel="icon" href="{{ asset('assets/images/favicon.ico') }}" type="image/x-icon">
     <!-- fontawesome icon -->
-    <link rel="stylesheet" href="{{url('/assets/fonts/fontawesome/css/fontawesome-all.min.css')}}">
+    {{-- <link rel="stylesheet" href="{{url('/assets/fonts/fontawesome/css/fontawesome-all.min.css')}}"> --}}
+    <link rel="stylesheet" href="{{ asset('assets/fonts/fontawesome/css/fontawesome-all.min.css') }}">
     <!-- animation css -->
-    <link rel="stylesheet" href="{{url('/assets/plugins/animation/css/animate.min.css')}}">
+    {{-- <link rel="stylesheet" href="{{url('/assets/plugins/animation/css/animate.min.css')}}"> --}}
+    <link rel="stylesheet" href="{{ asset('assets/plugins/animation/css/animate.min.css') }}">
     <!-- vendor css -->
-    <link rel="stylesheet" href="{{url('/assets/css/style.css')}}">
+    {{-- <link rel="stylesheet" href="{{url('/assets/css/style.css')}}"> --}}
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
 
 
 </head>
