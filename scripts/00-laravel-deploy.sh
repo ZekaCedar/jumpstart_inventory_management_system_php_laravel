14ad26a415654027809c57c8e26d096527de0220
Siti Zulaikha <siti@unblk.io>
2025-06-02 17:55:05 +0800

    Add built assets

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
-
-# Install Node.js (Render doesn't include it by default)
-curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
-apt-get install -y nodejs
-
 echo "Running composer..."
 composer install --no-dev --no-interaction --prefer-dist --optimize-autoloader --working-dir=/var/www/html
 
-echo "Installing npm dependencies..."
-npm ci --no-audit --prefer-offline
+# echo "Installing npm dependencies..."
+# npm ci --no-audit --prefer-offline
 
-echo "Running npm run build..."
-npm run build --if-present
+# echo "Running npm run build..."
+# npm run build --if-present
 
 echo "generating application key..."
 php artisan key:generate --show
